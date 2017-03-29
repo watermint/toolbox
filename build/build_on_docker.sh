01a57380506c789bb0ca3e05d5550f3c83c0942f
Takayuki Okazaki <watermint@users.noreply.github.com>
2017-03-29 10:52:53 +0900

    Major change in versioning scheme

--- build/build_on_docker.sh
+++ build/build_on_docker.sh
@@ -8,8 +8,16 @@ DIST_PATH=/dist
 TARGET_TOOLS="dupload dsharedlink dteammember dcmp"
 TARGET_PLATFORM="windows/386,windows/amd64,darwin/amd64,linux/386,linux/amd64"
 
-BUILD_VERSION=$(cat $PROJECT_ROOT/version)
+BUILD_MAJOR_VERSION=$(cat $PROJECT_ROOT/version)
 BUILD_HASH=$(cd $PROJECT_ROOT && git rev-parse HEAD)
+
+if [ "$TOOLBOX_BUILD_ID"x = ""x ]; then
+  TOOLBOX_BUILD_ID=0.0
+fi
+BUILD_VERSION=$BUILD_MAJOR_VERSION.$TOOLBOX_BUILD_ID
+
+echo --------------------
+echo BUILD: Start building version: $BUILD_VERSION
 
 echo --------------------
 echo BUILD: Testing..
@@ -29,12 +37,12 @@ for t in $TARGET_TOOLS; do
   if [ -e $PROJECT_ROOT/credentials.secret ]; then
     X_APP_CREDENTIALS=$(cat $PROJECT_ROOT/credentials.secret | xargs)
   fi
+  if [ "$TOOLBOX_APP_CREDENTIALS"x != ""x ]; then
+    X_APP_CREDENTIALS="$TOOLBOX_APP_CREDENTIALS"
+  fi
   LD_FLAGS="$X_APP_NAME $X_APP_VERSION $X_APP_HASH $X_APP_CREDENTIALS"
 
-  xgo --ldflags="$LD_FLAGS" -out $t-$BUILD_VERSION -targets $TARGET_PLATFORM github.com/watermint/toolbox/tools/$t 
-done
+  xgo --ldflags="$LD_FLAGS" -out $t-$BUILD_VERSION -targets $TARGET_PLATFORM github.com/watermint/toolbox/tools/$t
 
-echo --------------------
-echo BUILD: Packaging
-cd /build
-zip -9 -r $DIST_PATH/toolbox-$BUILD_VERSION.zip .
+  ( cd /build && zip -9 -r $DIST_PATH/$t-$BUILD_VERSION.zip $t-* )
+done
