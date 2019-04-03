9e2f006a1f87221de98b605d144ac4b1ebbe87b3
takayuki <watermint@users.noreply.github.com>
2019-04-03 13:10:16 +0900

    #59 : clean up permission for shared folders

--- domain/usecase/uc_team_migration/migration.go
+++ domain/usecase/uc_team_migration/migration.go
@@ -1178,6 +1178,52 @@ func (z *migrationImpl) Permissions(ctx Context) (err error) {
 }
 
 func (z *migrationImpl) Cleanup(ctx Context) (err error) {
+	// Clean up bridge permission
+	z.log().Info("Cleanup: clean up permissions of shared folders")
+	cleanupPermissionSharedFolder := func() error {
+		for _, folder := range ctx.SharedFolders() {
+			l := z.log().With(zap.String("name", folder.Name))
+			if folder.IsTeamFolder || folder.IsInsideTeamFolder {
+				l.Debug("Skip team folder & nested folder")
+				continue
+			}
+			owner, sameTeam := z.isTeamOwnedSharedFolder(ctx, folder.SharedFolderId)
+			if !sameTeam {
+				l.Debug("Skip non team owned folder")
+				continue
+			}
+			ownerMember, err := sv_member.New(z.ctxMgtDst).ResolveByEmail(owner.Email)
+			if err != nil {
+				l.Error("Unable to resolve folder owner user", zap.String("email", owner.Email), zap.Error(err))
+				return err
+			}
+
+			members := ctx.NamespaceMembers(folder.SharedFolderId)
+			isAdminExist := false
+			for _, member := range members {
+				if u, e := member.User(); e {
+					if u.TeamMemberId == ctx.AdminSrc().TeamMemberId {
+						isAdminExist = true
+					}
+				}
+			}
+			if isAdminExist {
+				l.Debug("Admin exists on the folder. Keep admin permission")
+			} else {
+				l.Info("Removing admin user from the folder", zap.String("admin", ctx.AdminDst().Email))
+				ctf := z.ctxFileDst.AsMemberId(ownerMember.TeamMemberId)
+				svm := sv_sharedfolder_member.NewBySharedFolderId(ctf, folder.SharedFolderId)
+				err = svm.Remove(sv_sharedfolder_member.RemoveByTeamMemberId(ctx.AdminDst().TeamMemberId))
+				if err != nil {
+					l.Error("Unable to remove admin from the folder", zap.String("admin", ctx.AdminDst().Email), zap.Error(err))
+				}
+			}
+		}
+		return nil
+	}
+	if err = cleanupPermissionSharedFolder(); err != nil {
+		return nil
+	}
 
 	return nil
 }
