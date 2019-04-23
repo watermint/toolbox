96ecff01adb56da2e209f5399505eb39f3f5407b
takayuki <watermint@users.noreply.github.com>
2019-04-23 15:02:32 +0900

    fix #104 : generate flagSet automatically

--- app/app_recipe/api_recipe_vo/vo_poc.go
+++ app/app_recipe/api_recipe_vo/vo_poc.go
@@ -3,12 +3,14 @@ package api_recipe_vo
 import (
 	"flag"
 	"github.com/iancoleman/strcase"
+	"github.com/watermint/toolbox/app"
 	"github.com/watermint/toolbox/app/app_recipe/api_recipe_msg"
 	"github.com/watermint/toolbox/app/app_recipe/api_recipe_report"
 	"github.com/watermint/toolbox/app/app_recipe/api_recpie_ctl"
 	"github.com/watermint/toolbox/domain/infra/api_context"
 	"go.uber.org/zap"
 	"reflect"
+	"runtime"
 )
 
 type Validator struct {
@@ -72,7 +74,20 @@ func WithBusinessManagement(exec func(rc ApiRecipeContext) error) Cook {
 	panic("implement me")
 }
 
-func MakeFlagSet(f *flag.FlagSet, vo interface{}) {
+type ValueContainer struct {
+	Values map[string]interface{}
+}
+
+func NewValueContainer(vo interface{}) *ValueContainer {
+	vc := &ValueContainer{
+		Values: make(map[string]interface{}),
+	}
+	vc.From(vo)
+	return vc
+}
+
+func (z *ValueContainer) From(vo interface{}) {
+	l := app.Root().Log()
 	vot := reflect.TypeOf(vo)
 	vov := reflect.ValueOf(vo)
 	if vot.Kind() == reflect.Ptr {
@@ -81,6 +96,7 @@ func MakeFlagSet(f *flag.FlagSet, vo interface{}) {
 	}
 
 	if vot.Kind() != reflect.Struct {
+		l.Error("ValueObject is not a struct", zap.String("name", vot.Name()), zap.String("pkg", vot.PkgPath()))
 		return
 	}
 
@@ -88,12 +104,96 @@ func MakeFlagSet(f *flag.FlagSet, vo interface{}) {
 	for i := 0; i < nf; i++ {
 		vof := vot.Field(i)
 		vvf := vov.Field(i)
-		kname := strcase.ToKebab(vof.Name)
+		kn := vof.Name
+		ll := l.With(zap.String("key", kn))
 
 		switch vof.Type.Kind() {
 		case reflect.Bool:
-			var dv bool
-			f.BoolVar(&dv, kname, vvf.Bool(), vof.Name)
+			v := vvf.Bool()
+			z.Values[kn] = &v
+		case reflect.Int:
+			v := vvf.Int()
+			z.Values[kn] = &v
+		case reflect.String:
+			v := vvf.String()
+			z.Values[kn] = &v
+		default:
+			ll.Debug("Unsupported type", zap.Any("kind", vof.Type.Kind()))
+		}
+	}
+}
+
+func (z *ValueContainer) Apply(vo interface{}) {
+	l := app.Root().Log()
+	defer func() {
+		if r := recover(); r != nil {
+			switch r0 := r.(type) {
+			case *runtime.TypeAssertionError:
+				l.Debug("Unable to convert type", zap.Error(r0))
+			default:
+				l.Debug("Unexpected error", zap.Any("r", r))
+			}
+		}
+	}()
+
+	vot := reflect.TypeOf(vo)
+	vov := reflect.ValueOf(vo)
+
+	// follow pointer
+	if vot.Kind() == reflect.Ptr {
+		vot = reflect.ValueOf(vo).Elem().Type()
+		vov = reflect.ValueOf(vo).Elem()
+	}
+
+	if vot.Kind() != reflect.Struct {
+		l.Error("ValueObject is not a struct", zap.String("name", vot.Name()), zap.String("pkg", vot.PkgPath()))
+		return
+	}
+
+	nf := vot.NumField()
+	for i := 0; i < nf; i++ {
+		vof := vot.Field(i)
+		vvf := vov.Field(i)
+		kn := vof.Name
+		ll := l.With(zap.String("key", kn))
+
+		switch vof.Type.Kind() {
+		case reflect.Bool:
+			if v, e := z.Values[kn]; e {
+				vvf.SetBool(*v.(*bool))
+			} else {
+				ll.Debug("Unable to find value")
+			}
+		case reflect.Int:
+			if v, e := z.Values[kn]; e {
+				vvf.SetInt(*v.(*int64))
+			} else {
+				ll.Debug("Unable to find value")
+			}
+		case reflect.String:
+			if v, e := z.Values[kn]; e {
+				vvf.SetString(*v.(*string))
+			} else {
+				ll.Debug("Unable to find value")
+			}
+		default:
+			ll.Debug("Not supported type", zap.Any("kind", vof.Type.Kind()))
+		}
+	}
+}
+
+func (z *ValueContainer) MakeFlagSet(f *flag.FlagSet) {
+	for n, d := range z.Values {
+		kf := strcase.ToKebab(n)
+		desc := n
+
+		switch dv := d.(type) {
+		case *bool:
+			f.BoolVar(dv, kf, *dv, desc)
+		case *int64:
+			f.Int64Var(dv, kf, *dv, desc)
+		case *string:
+			f.StringVar(dv, kf, *dv, desc)
 		}
 	}
 }
--- app/app_recipe/api_recipe_vo/vo_poc_test.go
+++ app/app_recipe/api_recipe_vo/vo_poc_test.go
@@ -9,24 +9,31 @@ func TestMakeFlagSet(t *testing.T) {
 	type FolderListVO struct {
 		Recursive    bool
 		NonRecursive bool
+		Depth        int
+		Filter       string
 	}
 
 	vo := &FolderListVO{
 		Recursive:    false,
 		NonRecursive: true,
+		Depth:        2,
+		Filter:       "",
 	}
 	f := flag.NewFlagSet("test", flag.ContinueOnError)
 
-	recursiveFound := false
-	nonRecursiveFound := false
+	vc := NewValueContainer(vo)
+	vc.MakeFlagSet(f)
 
-	MakeFlagSet(f, vo)
-
-	err := f.Parse([]string{"-recursive", "-non-recursive"})
+	err := f.Parse([]string{"-recursive", "-non-recursive=false", "-depth", "4", "-filter", "haystack"})
 	if err != nil {
 		t.Error(err)
 		return
 	}
+
+	recursiveFound := false
+	nonRecursiveFound := false
+	depthFound := false
+	filterFound := false
 
 	f.VisitAll(func(g *flag.Flag) {
 		switch g.Name {
@@ -41,6 +48,18 @@ func TestMakeFlagSet(t *testing.T) {
 				t.Error("invalid")
 			}
 			nonRecursiveFound = true
+
+		case "depth":
+			if g.DefValue != "2" {
+				t.Error("invalid")
+			}
+			depthFound = true
+
+		case "filter":
+			if g.DefValue != "" {
+				t.Error("invalid")
+			}
+			filterFound = true
 		}
 	})
 
@@ -49,5 +68,26 @@ func TestMakeFlagSet(t *testing.T) {
 	}
 	if !nonRecursiveFound {
 		t.Error("non recursive not found")
+	}
+	if !depthFound {
+		t.Error("depth not found")
+	}
+	if !filterFound {
+		t.Error("filter not found")
+	}
+
+	vc.Apply(vo)
+
+	if vo.Filter != "haystack" {
+		t.Error("invalid")
+	}
+	if vo.Depth != 4 {
+		t.Error("invalid")
+	}
+	if !vo.Recursive {
+		t.Error("invalid")
+	}
+	if vo.NonRecursive {
+		t.Error("invalid")
 	}
 }
