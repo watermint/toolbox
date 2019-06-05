1e391548377bbe4378a5ea5df7ba87b26ab46c4b
takayuki <watermint@users.noreply.github.com>
2019-06-05 23:45:41 +0900

    #86 : integrated command as `experimental`

--- tbx.go
+++ tbx.go
@@ -3,16 +3,24 @@ package main
 import (
 	"github.com/GeertJohan/go.rice"
 	"github.com/watermint/toolbox/app"
+	"github.com/watermint/toolbox/atbx/app_run"
 	"github.com/watermint/toolbox/cmd/cmd_root"
 	"os"
 )
 
 func main() {
-	bx := rice.MustFindBox("resources")
-	ec, err := app.NewExecContext(bx)
-	if err != nil {
-		return
+	switch {
+	case len(os.Args) > 1 && os.Args[1] == "experimental":
+		bx := rice.MustFindBox("atbx/resources")
+		app_run.Run(os.Args[2:], bx)
+
+	default:
+		bx := rice.MustFindBox("resources")
+		ec, err := app.NewExecContext(bx)
+		if err != nil {
+			return
+		}
+		cmds := cmd_root.NewCommands()
+		cmds.Exec(ec, os.Args)
 	}
-	cmds := cmd_root.NewCommands()
-	cmds.Exec(ec, os.Args)
 }
