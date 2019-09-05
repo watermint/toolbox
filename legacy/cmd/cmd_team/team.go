a34cebd37b25a4146fd76787f5c5c3040cc4368e
takayuki <watermint@users.noreply.github.com>
2019-09-05 23:26:05 +0900

    migrate team shared link expiration update from legacy

--- legacy/cmd/cmd_team/team.go
+++ legacy/cmd/cmd_team/team.go
@@ -6,7 +6,6 @@ import (
 	cmd_device2 "github.com/watermint/toolbox/legacy/cmd/cmd_team/cmd_device"
 	cmd_linkedapp2 "github.com/watermint/toolbox/legacy/cmd/cmd_team/cmd_linkedapp"
 	cmd_namespace2 "github.com/watermint/toolbox/legacy/cmd/cmd_team/cmd_namespace"
-	cmd_sharedlink2 "github.com/watermint/toolbox/legacy/cmd/cmd_team/cmd_sharedlink"
 )
 
 func NewCmdTeam() cmd2.Commandlet {
@@ -17,7 +16,6 @@ func NewCmdTeam() cmd2.Commandlet {
 			cmd_audit2.NewCmdTeamAudit(),
 			cmd_device2.NewCmdTeamDevice(),
 			cmd_linkedapp2.NewCmdMemberLinkedApp(),
-			cmd_sharedlink2.NewCmdTeamSharedLink(),
 			cmd_namespace2.NewCmdTeamNamespace(),
 		},
 	}
