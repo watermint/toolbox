d961fa953bd70057610b87e4fb5a2515dbbac113
takayuki <watermint@users.noreply.github.com>
2019-09-11 16:39:01 +0900

    fix #144 : column name of report

--- domain/model/mo_member/member.go
+++ domain/model/mo_member/member.go
@@ -8,22 +8,22 @@ import (
 
 type Member struct {
 	Raw             json.RawMessage
-	TeamMemberId    string `path:"profile.team_member_id"`
-	Email           string `path:"profile.email"`
-	EmailVerified   bool   `path:"profile.email_verified"`
-	Status          string `path:"profile.status.\\.tag"`
-	GivenName       string `path:"profile.name.given_name"`
-	Surname         string `path:"profile.name.surname"`
-	FamiliarName    string `path:"profile.name.familiar_name"`
-	DisplayName     string `path:"profile.name.display_name"`
-	AbbreviatedName string `path:"profile.name.abbreviated_name"`
-	MemberFolderId  string `path:"profile.member_folder_id"`
-	ExternalId      string `path:"profile.external_id"`
-	AccountId       string `path:"profile.account_id"`
-	PersistentId    string `path:"profile.persistent_id"`
-	JoinedOn        string `path:"profile.joined_on"`
-	Role            string `path:"role.\\.tag"`
-	Tag             string `path:"\\.tag"`
+	TeamMemberId    string `path:"profile.team_member_id" json:"team_member_id"`
+	Email           string `path:"profile.email" json:"email"`
+	EmailVerified   bool   `path:"profile.email_verified" json:"email_verified"`
+	Status          string `path:"profile.status.\\.tag" json:"status"`
+	GivenName       string `path:"profile.name.given_name" json:"given_name"`
+	Surname         string `path:"profile.name.surname" json:"surname"`
+	FamiliarName    string `path:"profile.name.familiar_name" json:"familiar_name"`
+	DisplayName     string `path:"profile.name.display_name" json:"display_name"`
+	AbbreviatedName string `path:"profile.name.abbreviated_name" json:"abbreviated_name"`
+	MemberFolderId  string `path:"profile.member_folder_id" json:"member_folder_id"`
+	ExternalId      string `path:"profile.external_id" json:"external_id"`
+	AccountId       string `path:"profile.account_id" json:"account_id"`
+	PersistentId    string `path:"profile.persistent_id" json:"persistent_id"`
+	JoinedOn        string `path:"profile.joined_on" json:"joined_on"`
+	Role            string `path:"role.\\.tag" json:"role"`
+	Tag             string `path:"\\.tag" json:"tag"`
 }
 
 func (z *Member) Profile() *mo_profile.Profile {
