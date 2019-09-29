1b6584ef3d11609ae7b9a5fe2d6727ac11c3c4b5
takayuki <watermint@users.noreply.github.com>
2019-09-29 12:38:37 +0900

    circleci : release process

--- release.sh
+++ release.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
 # Prepare for release
 
-if [ x"master" != x"$CIRCLE_BRANCH" ]; then
-  echo Skip release testing: current branch "$CIRCLE_BRANCH"
+#if [ x"master" != x"$CIRCLE_BRANCH" ]; then
+#  echo Skip release testing: current branch "$CIRCLE_BRANCH"
+#  exit 0
+#fi
+
+if [ x"" == x"$TOOLBOX_TESTRESOURCE_URL" ]; then
+  echo Skip testing with supplemental test resource, test resource url missing
+  exit 0
+fi
+if [ x"" == x"$TOOLBOX_TESTRESOURCE" ]; then
+  echo Skip testing with supplemental test resource, test resource path missing
   exit 0
 fi
 
-if [ x"" == x"$TOOLBOX_TESTRESOURCE" ]; then
-  echo Skip testing with supplemental test resource
-  exit 0
-fi
+curl -L "$TOOLBOX_TESTRESOURCE_URL" > "$TOOLBOX_TESTRESOURCE"
