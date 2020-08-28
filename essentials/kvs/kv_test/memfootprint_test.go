a34ac0f379ab486624c8d539fb554fdbaa130a58
takayuki <watermint@users.noreply.github.com>
2020-08-28 13:44:04 +0900

    #426 : memory benchmark

--- essentials/kvs/kv_test/memfootprint_test.go
+++ essentials/kvs/kv_test/memfootprint_test.go
@@ -3,6 +3,7 @@ package kv
 import (
 	"fmt"
 	"github.com/watermint/toolbox/essentials/kvs/kv_kvs"
+	"github.com/watermint/toolbox/essentials/kvs/kv_storage"
 	"github.com/watermint/toolbox/essentials/kvs/kv_storage_impl"
 	"github.com/watermint/toolbox/essentials/log/esl"
 	"github.com/watermint/toolbox/infra/control/app_control"
@@ -11,58 +12,75 @@ import (
 	"testing"
 )
 
-func BenchmarkMemoryFootprint(b *testing.B) {
-	dataSizeMin := 1024
-	dataSizeMax := 16384
+func benchmarkLoadTest(b *testing.B, ctl app_control.Control, db kv_storage.Storage) {
+	dataSizeMin := 2 << 10
+	dataSizeMax := 2 << 16
 	mul := 10
+	var err error
+
+	l := ctl.Log()
+	l.Info("Benchmark", esl.Int("N", b.N))
+	for i := 0; i < b.N*mul; i++ {
+		err = db.Update(func(bmf kv_kvs.Kvs) error {
+			key := fmt.Sprintf("k%x", i)
+			dataSize := rand.Intn(dataSizeMax) + dataSizeMin
+			data := make([]byte, dataSize)
+			if n, err := rand.Read(data); err != nil || n != dataSize {
+				b.Error(err, n)
+				return err
+			}
+
+			if err = bmf.PutBytes(key, data); err != nil {
+				b.Error(err)
+				return err
+			}
+			return nil
+		})
+	}
+
+	for i := 0; i < b.N*mul; i++ {
+		err = db.View(func(bmf kv_kvs.Kvs) error {
+			key := fmt.Sprintf("k%x", i)
+			dataSize := rand.Intn(dataSizeMax) + dataSizeMin
+			data := make([]byte, dataSize)
+			if n, err := rand.Read(data); err != nil || n != dataSize {
+				b.Error(err, n)
+				return err
+			}
+
+			if _, err = bmf.GetBytes(key); err != nil {
+				b.Error(err)
+				return err
+			}
+			return nil
+		})
+	}
+
+	if err != nil {
+		b.Error(err)
+	}
+}
+
+func BenchmarkMemoryFootprintBadger(b *testing.B) {
 	qtr_endtoend.BenchmarkWithControl(b, func(ctl app_control.Control) {
-		var err error
-		db := kv_storage_impl.InternalNewBadger("benchmark-memory-footprint")
+		db := kv_storage_impl.InternalNewBadger("benchmark-memory-badger")
 		if err := db.Open(ctl); err != nil {
 			b.Error(err)
 			return
 		}
-		defer db.Close()
-		l := ctl.Log()
-		l.Info("Benchmark", esl.Int("N", b.N))
-		for i := 0; i < b.N*mul; i++ {
-			err = db.Update(func(bmf kv_kvs.Kvs) error {
-				key := fmt.Sprintf("k%x", i)
-				dataSize := rand.Intn(dataSizeMax) + dataSizeMin
-				data := make([]byte, dataSize)
-				if n, err := rand.Read(data); err != nil || n != dataSize {
-					b.Error(err, n)
-					return err
-				}
+		benchmarkLoadTest(b, ctl, db)
+		db.Close()
+	})
+}
 
-				if err = bmf.PutBytes(key, data); err != nil {
-					b.Error(err)
-					return err
-				}
-				return nil
-			})
-		}
-
-		for i := 0; i < b.N*mul; i++ {
-			err = db.View(func(bmf kv_kvs.Kvs) error {
-				key := fmt.Sprintf("k%x", i)
-				dataSize := rand.Intn(dataSizeMax) + dataSizeMin
-				data := make([]byte, dataSize)
-				if n, err := rand.Read(data); err != nil || n != dataSize {
-					b.Error(err, n)
-					return err
-				}
-
-				if _, err = bmf.GetBytes(key); err != nil {
-					b.Error(err)
-					return err
-				}
-				return nil
-			})
-		}
-
-		if err != nil {
+func BenchmarkMemoryFootprintBitcask(b *testing.B) {
+	qtr_endtoend.BenchmarkWithControl(b, func(ctl app_control.Control) {
+		db := kv_storage_impl.InternalNewBitcask("benchmark-memory-bitcask")
+		if err := db.Open(ctl); err != nil {
 			b.Error(err)
+			return
 		}
+		benchmarkLoadTest(b, ctl, db)
+		db.Close()
 	})
 }
