be64403d8880ca060dcb7c5182e8f78aa37a862c
takayuki <watermint@users.noreply.github.com>
2020-12-07 22:05:10 +0900

    #471 : Change test sequence

--- test/test.sh
+++ test/test.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+if [ x"" = x"$PROJECT_ROOT" ]; then
+  PROJECT_ROOT=$(dirname $0)
+fi
+cd "$PROJECT_ROOT"
+
 OUT_RESULTS=test/results
 OUT_TEST=$OUT_RESULTS/all.out
 OUT_TEST_REPORT=$OUT_RESULTS/all.xml
@@ -12,13 +17,14 @@ mkdir -p $OUT_RESULTS
 echo "" >$OUT_COVERAGE
 echo "" >$OUT_TEST
 
-go test -short -v -coverpkg=./... -coverprofile=$OUT_PROFILE -covermode=atomic ./... 2>&1 > "$OUT_TEST"
+go test -short -v -coverpkg=github.com/watermint/toolbox/... -coverprofile=$OUT_PROFILE -covermode=atomic github.com/watermint/toolbox/... 2>&1 > "$OUT_TEST"
 if [ "$?" -ne "0" ]; then
   echo Test failed: $?
   exit 1
 fi
 
-hash go-junit-report 2>/dev/null
-if [ "$?" -eq "0" ]; then
-  cat $OUT_TEST | go-junit-report >$OUT_TEST_REPORT
-fi
+#hash go-junit-report 2>/dev/null
+#if [ "$?" -eq "0" ]; then
+#  cat $OUT_TEST | go-junit-report >$OUT_TEST_REPORT
+#fi
+
