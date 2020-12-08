1e3d0959b4d78194c97beec5065e1753e9cb8c8e
takayuki <watermint@users.noreply.github.com>
2020-12-08 18:23:46 +0900

    #471 : Change test sequence

--- test/test.sh
+++ test/test.sh
@@ -15,7 +15,7 @@ go test -v -covermode=atomic -coverprofile=$TEST_PROFILE  ./... > "$TEST_OUT" 2>
 TEST_EXIT_CODE=$?
 
 if [ "$CIRCLE_BUILD_NUM"x != ""x ]; then
-    go run tbx.go file sync up -local-path $TEST_DEBUG -dropbox-path /watermint-toolbox-build/test-logs/$CIRCLE_BUILD_NUM
+    go run tbx.go dev ci artifact up -budget-memory low -local-path $TEST_DEBUG -dropbox-path /watermint-toolbox-build/test-logs/$CIRCLE_BUILD_NUM -peer-name deploy
 fi
 
 if [ $TEST_EXIT_CODE -ne 0 ]; then
