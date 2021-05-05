b5ab011b0da52ca560f89f2bd436cd42d9215686
Takayuki Okazaki <watermint@users.noreply.github.com>
2021-05-05 16:53:45 +0900

    #523 : fix links in README

--- recipe/license_test.go
+++ recipe/license_test.go
@@ -1,14 +1,10 @@
 package recipe
 
 import (
-	"github.com/watermint/toolbox/quality/infra/qt_endtoend"
 	"github.com/watermint/toolbox/quality/recipe/qtr_endtoend"
 	"testing"
 )
 
 func TestLicense_Exec(t *testing.T) {
-	if qt_endtoend.IsSkipEndToEndTest() {
-		return
-	}
 	qtr_endtoend.TestRecipe(t, &License{})
 }
