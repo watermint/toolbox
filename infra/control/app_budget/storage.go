e483daf1a8be3bbeba5b9a4910e5e45e6785ecdc
takayuki <watermint@users.noreply.github.com>
2022-06-24 23:17:16 +0900

    fix #595 : update log quota

--- infra/control/app_budget/storage.go
+++ infra/control/app_budget/storage.go
@@ -3,19 +3,16 @@ package app_budget
 import (
 	"github.com/watermint/toolbox/essentials/log/esl"
 	"github.com/watermint/toolbox/essentials/log/esl_rotate"
-	"math"
 )
 
 const (
-	unlimitedChunkSize = math.MaxInt64 // no rotate
+	logChunkSize       = 100 * 1048576 // 100MiB
 	unlimitedQuota     = esl_rotate.UnlimitedQuota
 	unlimitedNumBackup = esl_rotate.UnlimitedBackups
-	standardChunkSize  = 100 * 1048576  // 100MiB
 	standardQuota      = 5 * 1073741824 // 5GiB * 2 = 10GiB
-	standardNumBackup  = standardQuota * 2 / standardChunkSize
-	lowChunkSize       = 10 * 1048576  // 10MiB
+	standardNumBackup  = standardQuota * 2 / logChunkSize
 	lowQuota           = 500 * 1048576 // 500MiB * 2 = 1GiB
-	lowNumBackup       = lowQuota * 2 / lowChunkSize
+	lowNumBackup       = lowQuota * 2 / logChunkSize
 
 	BudgetLow       Budget = "low"
 	BudgetNormal    Budget = "normal"
@@ -34,11 +31,11 @@ var (
 func StorageBudget(budget Budget) (chunkSize, quota int64, numBackup int) {
 	switch budget {
 	case BudgetLow:
-		return lowChunkSize, lowQuota, lowNumBackup
+		return logChunkSize, lowQuota, lowNumBackup
 	case BudgetNormal:
-		return standardChunkSize, standardQuota, standardNumBackup
+		return logChunkSize, standardQuota, standardNumBackup
 	case BudgetUnlimited:
-		return unlimitedChunkSize, unlimitedQuota, unlimitedNumBackup
+		return logChunkSize, unlimitedQuota, unlimitedNumBackup
 	default:
 		l := esl.Default()
 		l.Error("Unsupported budget type, fallback to BudgetNormal", esl.String("budget", string(budget)))
