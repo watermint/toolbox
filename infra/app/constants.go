5888c2cfe208311b129ee7ac60e0ef8793db4cbf
Takayuki Okazaki <watermint@users.noreply.github.com>
2023-01-16 13:40:25 +0900

    #676 : revert badge

--- infra/app/constants.go
+++ infra/app/constants.go
@@ -70,7 +70,7 @@ const (
 const (
 	// Project status badge
 	ProjectStatusBadge = `
-[![Go](https://github.com/watermint/toolbox/actions/workflows/go.yml/badge.svg?branch=main)](https://github.com/watermint/toolbox/actions/workflows/go.yml)
+[![CircleCI](https://circleci.com/gh/watermint/toolbox.svg?style=shield)](https://circleci.com/gh/watermint/toolbox)
 [![codecov](https://codecov.io/gh/watermint/toolbox/branch/main/graph/badge.svg?token=CrE8reSVvE)](https://codecov.io/gh/watermint/toolbox)
 `
 
