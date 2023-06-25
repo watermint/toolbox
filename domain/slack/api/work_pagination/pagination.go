4bb536c8acde6a1449f69d39b051b853c43cda2e
Takayuki Okazaki <watermint@users.noreply.github.com>
2023-06-25 09:24:48 +0900

    #726 : fix slack pagination

--- domain/slack/api/work_pagination/pagination.go
+++ domain/slack/api/work_pagination/pagination.go
@@ -51,8 +51,11 @@ func (z pgImpl) OnData(path string, handler func(entry es_json.Json) error) erro
 			Cursor: cursor,
 		}
 		data := make([]api_request.RequestDatum, 0)
-		data = append(data, z.data...)
-		data = append(data, api_request.Query(pp))
+		if cursor == "" {
+			data = append(data, z.data...)
+		} else {
+			data = append(data, api_request.Query(pp))
+		}
 		res := z.ctx.Get(z.endpoint, data...)
 		if err, fail := res.Failure(); fail {
 			ll.Debug("Failure response", esl.Error(err))
