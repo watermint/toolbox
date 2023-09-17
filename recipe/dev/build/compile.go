980a2064e86773146a36b6739db58cc129bace9c
Takayuki Okazaki <watermint@users.noreply.github.com>
2023-09-17 13:48:36 +0900

    #747 : linux/arm64 binary build failure

--- recipe/dev/build/compile.go
+++ recipe/dev/build/compile.go
@@ -81,7 +81,7 @@ func (z *Compile) compileLinux() []string {
 	case "arm64":
 		return []string{
 			z.goBuildCgoEnabled(map[string]string{
-				//"CC":     "aarch64-linux-gnu-gcc-6",
+				"CC": "aarch64-linux-musl-gcc",
 				//"CXX":    "aarch64-linux-gnu-g++-6",
 				"GOOS":   "linux",
 				"GOARCH": "arm64",
