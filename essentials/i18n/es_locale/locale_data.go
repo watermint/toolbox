d99bba6962f10f53d091c048a26bb0bd7eed3607
Takayuki Okazaki <watermint@users.noreply.github.com>
2023-11-27 00:27:59 +0900

    #780 : code clean up

--- essentials/i18n/es_locale/locale_data.go
+++ essentials/i18n/es_locale/locale_data.go
@@ -1,7 +1,7 @@
 package es_locale
 
 import (
-	"github.com/watermint/toolbox/essentials/strings/es_regexp2"
+	"github.com/watermint/toolbox/essentials/strings/es_regexp"
 	"regexp"
 )
 
@@ -9,7 +9,7 @@ var (
 	bcp47Lang = regexp.MustCompile(`(?P<lang>([A-Za-z]{2,3}))(-(.*))?(\.(?P<codepage>(.+)))?`)
 
 	// https://stackoverflow.com/questions/7035825/regular-expression-for-a-language-tag-as-defined-by-bcp47
-	bcp47Re = es_regexp2.MustNew(`^((?P<grandfathered>(en-GB-oed|i-ami|i-bnn|i-default|i-enochian|i-hak|i-klingon|i-lux|i-mingo|i-navajo|i-pwn|i-tao|i-tay|i-tsu|sgn-BE-FR|sgn-BE-NL|sgn-CH-DE)|(art-lojban|cel-gaulish|no-bok|no-nyn|zh-guoyu|zh-hakka|zh-min|zh-min-nan|zh-xiang))|((?P<language>([A-Za-z]{2,3}(-(?P<extlang>[A-Za-z]{3}(-[A-Za-z]{3}){0,2}))?)|[A-Za-z]{4}|[A-Za-z]{5,8})(-(?P<script>[A-Za-z]{4}))?(-(?P<region>[A-Za-z]{2}|[0-9]{3}))?(-(?P<variant>[A-Za-z0-9]{5,8}|[0-9][A-Za-z0-9]{3}))*(-(?P<extension>[0-9A-WY-Za-wy-z](-[A-Za-z0-9]{2,8})+))*(-(?P<privateUse>x(-[A-Za-z0-9]{1,8})+))?)|(?P<privateUse2>x(-[A-Za-z0-9]{1,8})+))(\.(?P<codepage>(.+)))?$`)
+	bcp47Re = es_regexp.MustNew(`^((?P<grandfathered>(en-GB-oed|i-ami|i-bnn|i-default|i-enochian|i-hak|i-klingon|i-lux|i-mingo|i-navajo|i-pwn|i-tao|i-tay|i-tsu|sgn-BE-FR|sgn-BE-NL|sgn-CH-DE)|(art-lojban|cel-gaulish|no-bok|no-nyn|zh-guoyu|zh-hakka|zh-min|zh-min-nan|zh-xiang))|((?P<language>([A-Za-z]{2,3}(-(?P<extlang>[A-Za-z]{3}(-[A-Za-z]{3}){0,2}))?)|[A-Za-z]{4}|[A-Za-z]{5,8})(-(?P<script>[A-Za-z]{4}))?(-(?P<region>[A-Za-z]{2}|[0-9]{3}))?(-(?P<variant>[A-Za-z0-9]{5,8}|[0-9][A-Za-z0-9]{3}))*(-(?P<extension>[0-9A-WY-Za-wy-z](-[A-Za-z0-9]{2,8})+))*(-(?P<privateUse>x(-[A-Za-z0-9]{1,8})+))?)|(?P<privateUse2>x(-[A-Za-z0-9]{1,8})+))(\.(?P<codepage>(.+)))?$`)
 )
 
 var (
--- essentials/strings/es_regexp2/regexp.go
+++
@@ -1,5 +0,0 @@
-package es_regexp2
-
-type Regexp interface {
-	MatchSubExp(s string) (matches map[string]string, match bool)
-}
