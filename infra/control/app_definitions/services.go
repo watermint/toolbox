6b14a0e6a2718df28b9e02316cd33be21f6c8e3f
Takayuki Okazaki <watermint@users.noreply.github.com>
2024-08-15 21:11:29 +0900

    #835 : remove deprecated

--- infra/control/app_definitions/services.go
+++ infra/control/app_definitions/services.go
@@ -5,10 +5,6 @@ const (
 	ServiceDropboxTeam       = "dropbox_team"
 	ServiceDropboxSign       = "dropbox_sign"
 	ServiceGithub            = "github"
-	ServiceGoogleMail        = "google_mail2024"     // Adding 2024 to avoid conflict with existing auth data #777
-	ServiceGoogleSheets      = "google_sheets2024"   // Adding 2024 to avoid conflict with existing auth data #777
-	ServiceGoogleCalendar    = "google_calendar2024" // Adding 2024 to avoid conflict with existing auth data #777
-	ServiceGoogleTranslate   = "google_translate"
 	ServiceAsana             = "asana"
 	ServiceDeepl             = "deepl"
 	ServiceSlack             = "slack"
@@ -24,9 +20,6 @@ var (
 		ServiceDeepl,
 		ServiceFigma,
 		ServiceGithub,
-		ServiceGoogleCalendar,
-		ServiceGoogleMail,
-		ServiceGoogleSheets,
 		ServiceSlack,
 	}
 )
