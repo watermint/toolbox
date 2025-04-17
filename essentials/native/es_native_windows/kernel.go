4a80eaf955f872d766b83ed5aae12640b64c1933
Takayuki Okazaki <watermint@users.noreply.github.com>
2025-04-18 00:10:21 +0900

    #898 : Remove custom error handling code and simplify error propagation
    This commit removes redundant custom error wrapping and type-checking logic for desktop and kernel operations. It simplifies error handling by propagating standard errors directly, improving maintainability and reducing complexity.

--- essentials/native/es_native_windows/kernel.go
+++ essentials/native/es_native_windows/kernel.go
@@ -6,29 +6,29 @@ package es_native_windows
 import "syscall"
 
 type Kernel interface {
-    Call(procName string, args ...uintptr) (r1, r2 uintptr, err error)
+	Call(procName string, args ...uintptr) (r1, r2 uintptr, err error)
 }
 
 var (
-    Kernel32 Kernel = &kernelWrapper{}
+	Kernel32 Kernel = &kernelWrapper{}
 )
 
 type kernelWrapper struct {
 }
 
 func (z kernelWrapper) Call(procName string, args ...uintptr) (r1, r2 uintptr, err error) {
-    k32, resolveErr := syscall.LoadDLL("kernel32")
-    if resolveErr != nil {
-        return 0, 0, resolveErr
-    }
-    proc, resolveErr := k32.FindProc(procName)
-    if resolveErr != nil {
-        return 0, 0, resolveErr
-    }
+	k32, resolveErr := syscall.LoadDLL("kernel32")
+	if resolveErr != nil {
+		return 0, 0, resolveErr
+	}
+	proc, resolveErr := k32.FindProc(procName)
+	if resolveErr != nil {
+		return 0, 0, resolveErr
+	}
 
-    r1, r2, lastErr := proc.Call(args...)
-    if lastErr != nil {
-        return r1, r2, lastErr
-    }
-    return r1, r2, nil
+	r1, r2, lastErr := proc.Call(args...)
+	if lastErr != nil {
+		return r1, r2, lastErr
+	}
+	return r1, r2, nil
 }
